f4444690ad19a3ae59165d3689d47a7b98d6ba76
sintayehu-dev <sintayehubelfa23@gmail.com>
2026-04-08 20:46:56 -0700

    chore(deps): remove unused dependencies and update package versions
    - Remove flutter_tts plugin from macOS and Windows registrants
    - Remove speech_to_text_macos plugin from macOS registrant
    - Remove unused direct dependencies: audio_waveforms, country_code_picker, equatable, fl_chart, flash, flutter_map, flutter_spinkit, flutter_svg, health
    - Remove transitive dependencies: carp_serializable, diacritic
    - Update connectivity_plus from 6.1.4 to 6.1.5
    - Update connectivity_plus_platform_interface from 2.0.1 to 2.1.0
    - Update dbus from 0.7.11 to 0.7.12
    - Downgrade http from direct to transitive dependency
    - Clean up generated plugin registrants for macOS and Windows platforms

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -12,12 +12,10 @@ import file_selector_macos
 import firebase_auth
 import firebase_core
 import firebase_storage
-import flutter_tts
 import google_sign_in_ios
 import mobile_scanner
 import path_provider_foundation
 import shared_preferences_foundation
-import speech_to_text_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
@@ -27,10 +25,8 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
-  FlutterTtsPlugin.register(with: registry.registrar(forPlugin: "FlutterTtsPlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
-  SpeechToTextMacosPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextMacosPlugin"))
 }
